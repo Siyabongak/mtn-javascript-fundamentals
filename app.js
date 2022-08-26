f66317c202a7e54949e63d5be499a48096252258
Siyabongak <88520913+Siyabongak@users.noreply.github.com>
2022-08-26 10:21:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+// User information M2-Assessment 1 (a)
+let name = "Siyabonga";
+let institution = "Ikwezi Mining";
+let gitHub = "Siyabongak";
+
+let userInfo = `Greetings my name is ${name}, an employee at ${institution} and my Github username is ${gitHub}`;
+
+console.log(userInfo);
